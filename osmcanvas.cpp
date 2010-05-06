9f38a2240c4c4e80fa8c43626ad04b88b3f9961c
martijn <martijn@martijn-laptop.(none)>
2010-05-07 01:22:20 +0200

    moved TileDrawer to its own file

--- osmcanvas.cpp
+++ osmcanvas.cpp
@@ -1,6 +1,7 @@
 #include "osmcanvas.h"
 #include "parse.h"
 #include "rulecontrol.h"
+#include "tiledrawer.h"
 
 BEGIN_EVENT_TABLE(OsmCanvas, Canvas)
 	EVT_MOUSEWHEEL(OsmCanvas::OnMouseWheel)
@@ -13,103 +14,6 @@ BEGIN_EVENT_TABLE(OsmCanvas, Canvas)
 	EVT_MOTION(OsmCanvas::OnMouseMove)
 	EVT_TIMER(-1, OsmCanvas::OnTimer)
 END_EVENT_TABLE()
-
-
-TileWay::TileWay(OsmWay *way, TileList *allTiles, TileWay *next)
-	: ListObject(next)
-{
-	m_way = way;
-	m_tiles = allTiles;
-	if (m_tiles)
-		m_tiles->Ref();
-}
-
-TileWay::~TileWay()
-{
-	m_tiles->UnRef();
-}
-
-bool TileDrawer::RenderTiles(wxApp *app, OsmCanvas *canvas, double lon, double lat, double w, double h, bool restart)
-{
-	bool mustCancel = false;
-
-	DRect bb(lon, lat, w, h);
-
-//	canvas->Rect(wxEmptyString, bb, 0, 255,0,0);
-
-	if (restart || !m_visibleTiles)
-	{
-		if (m_visibleTiles)
-			m_visibleTiles->DestroyList();
-
-		if (m_renderedTiles)
-			m_renderedTiles->DestroyList();
-
-		m_renderedTiles = NULL;
-		m_visibleTiles = GetTiles(bb);
-
-		m_curTile = m_visibleTiles;
-		m_curLayer = 0;
-	}
-
-	if (!m_visibleTiles)
-	{
-		return false;
-	}
-
-	while (!mustCancel && m_curLayer < NUMLAYERS)
-	{
-		while (m_curTile && !mustCancel)
-		{
-			OsmTile *t = m_curTile->m_tile;
-//			canvas->Rect(wxEmptyString, *t, -1, 0,255,0);
-			if (t->OverLaps(bb))
-			{
-				for (TileWay *w = t->m_ways; w && !mustCancel; w = static_cast<TileWay *>(w->m_next))
-				{
-
-					bool already = false;
-					// loop over all tiles that contaoin this way
-					for (TileList *a = w->m_tiles; a; a = static_cast<TileList *>(a->m_next))
-					{
-						// loop over all tiles already drawn
-						for (TileList *o = m_renderedTiles; o ; o = static_cast<TileList *>(o->m_next))
-						{
-							 if (o->m_tile->m_id == a->m_tile->m_id)
-							 {
-								already = true;
-								break;
-							 }
-						}
-	
-						if (already)
-						{
-							break;
-						}
-					}
-	
-					if (!already)
-					{
-						RenderWay(w->m_way, m_curLayer);
-					}
-				}	// for way
-			}  // if overlaps
-			mustCancel = app->Pending();
-			m_renderedTiles = new TileList(t, m_renderedTiles);
-			m_curTile = static_cast<TileList *>(m_curTile->m_next);
-		}	 // while curTile
-		
-		if (!m_curTile)
-		{
-			m_curLayer++;
-			m_curTile = m_visibleTiles;
-			m_renderedTiles->DestroyList();
-			m_renderedTiles = NULL;
-		}
-	}  // while curLayer
-
-	return !mustCancel;
-}
 
 
 OsmCanvas::OsmCanvas(wxApp * app, wxWindow *parent, wxString const &fileName)
@@ -191,90 +95,6 @@ OsmCanvas::OsmCanvas(wxApp * app, wxWindow *parent, wxString const &fileName)
 
 	m_timer.Start(100);
 }
-
-
-void TileDrawer::Rect(wxString const &text, double lon1, double lat1, double lon2, double lat2, double border, int r, int g, int b)
-{
-	m_renderer->SetLineColor(r,g,b);
-	m_renderer->Rect(lon1, lat1, lon2 - lon1, lat2 - lat1, border , r, g, b);
-	m_renderer->DrawCenteredText(text.mb_str(wxConvUTF8), (lon1 + lon2)/2, (lat1 + lat2)/2, 0, r, g, b);
-}
-
-// render using default colours. should plug in rule engine here
-void TileDrawer::RenderWay(OsmWay *w, int curLayer)
-{
-
-	if ((!m_drawRule) || m_drawRule->Evaluate(w))
-	{
-		wxColour c = wxColour(150,150,150);
-		bool poly = false;
-		int layer = 1;
-		if (m_colorRules)
-		{
-			for (int i = 0; i < m_colorRules->m_num; i++)
-			{
-				if (m_colorRules->m_rules[i]->Evaluate(w))
-				{
-					c = m_colorRules->m_pickers[i]->GetColour();
-					poly = m_colorRules->m_checkBoxes[i]->IsChecked();
-					layer = m_colorRules->m_layers[i]->GetSelection();
-					break; // stop after first match
-				}
-			}
-		}
-		if (curLayer == layer)
-		{
-			RenderWay(w, c, poly, c);
-		}
-	}
-}
-
-
-void TileDrawer::RenderWay(OsmWay *w, wxColour lineColour, bool poly, wxColour fillColour)
-{
-
-
-	m_renderer->SetLineColor(lineColour.Red(), lineColour.Green(), lineColour.Blue());
-	m_renderer->SetFillColor(fillColour.Red(), fillColour.Green(), fillColour.Blue());
-
-	if (!poly)
-	{
-		m_renderer->Begin(Renderer::R_LINE);
-		for (unsigned j = 0; j < w->m_numResolvedNodes; j++)
-		{
-			OsmNode *node = w->m_resolvedNodes[j];
-		
-			if (node)
-			{
-				m_renderer->AddPoint(node->m_lon, node->m_lat);
-			}
-			else
-			{
-				m_renderer->End();
-				m_renderer->Begin(Renderer::R_LINE);
-			}
-		
-		}
-		m_renderer->End();
-	}
-	else
-	{
-		m_renderer->Begin(Renderer::R_POLYGON);
-		for (unsigned j = 0; j < w->m_numResolvedNodes; j++)
-		{
-			OsmNode *node = w->m_resolvedNodes[j];
-		
-			if (node)
-			{
-				m_renderer->AddPoint(node->m_lon, node->m_lat);
-			}
-			
-		}
-		m_renderer->End();
-	}
-}
-
-
 
 void OsmCanvas::Render(bool force)
 {
@@ -404,3 +224,10 @@ void OsmCanvas::SetupRenderer()
 
 	m_renderer.Setup(&m_backBuffer, DRect(m_xOffset, m_yOffset, sw, sh));
 }
+
+void OsmCanvas::SetRuleControls(RuleControl *rules, ColorRules *colors)
+{
+	m_tileDrawer->SetDrawRuleControl(rules);
+	m_tileDrawer->SetColorRules(colors);
+}
+
